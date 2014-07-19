15c40a7a92bd0f6e18f7ee9c4c669751819edab7
Jia Jyun,Syu <abc810601@gmail.com>
2014-07-19 11:41:30 +0800

    Update BaseSoldier.cs

--- BaseSoldier.cs
+++ BaseSoldier.cs
@@ -33,7 +33,7 @@ public class BaseSoldier : MonoBehaviour {
 		int tmpDamage = (int)p_param[0];
 		m_hp -= tmpDamage;
 		Debug.Log(string.Format("Soldier {0} Get {1} Damage",m_Name,tmpDamage.ToString()));
-		if(m_hp <=5)
+		if(m_hp <=5 || m_cb != null)
 			m_cb(this);
 	}
 
